c0cbb21dbe73e0e674f10b03c3d804b9200e564c
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 06:55:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,11 +9,3 @@ def my_all?(collection)
 end
 
 
-def my_all?(collection)
-  i = 0
-  block_return_values = []
-  while i < collection.length
-    block_return_values << yield(collection[i])
-    i += 1
-  end
-end
